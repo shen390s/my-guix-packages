c55d21fa5c8a7dad13f186c3ca02f198f3fff065
Rongsong Shen <rongsong.shen@akrostar-tech.com>
2025-02-10 13:01:37 +0800

    save work status

--- shen390s/packages/gcc-glibc-toolchains.scm
+++ shen390s/packages/gcc-glibc-toolchains.scm
@@ -3,13 +3,16 @@
   #:use-module (guix packages)
   #:use-module (guix download)
   #:use-module (guix build-system gnu)
+  #:use-module (gnu packages)
   #:use-module (gnu packages gcc)
   #:use-module (gnu packages base)
+  #:use-module (gnu packages commencement)
   #:use-module (guix licenses)
   #:use-module (gnu packages bash))
 
 (define-public glibc-2.17
   (package
+    (inherit glibc-2.29)
     (name "glibc")
     (version "2.17")
     (source (origin
@@ -17,18 +20,14 @@
              (uri (string-append "http://ftp.gnu.org/gnu/libc/glibc-" version ".tar.gz"))
              (sha256
               (base32
-               "1fnvqnk2l12p0bcz8z24dpm35hn8d0m7lsg4wxg56hdh6kq43khp"))))
-    (build-system gnu-build-system)
-    (inputs (list bash))
-    (synopsis "GNU C Library")
-    (description "The GNU C Library provides the core libraries for the Linux operating system.")
-    (home-page "https://www.gnu.org/software/libc/")
-    (license lgpl2.0+)))
+               "0ym3zk9ii64279wgw7pw9xkbxczy2ci7ka6mnfs05rhlainhicm3"))))
+    (arguments
+      `(#:out-of-source? #t))))
 
 (define-public gcc-with-glibc
   (package
     (name "gcc")
-    (version "10.2.0") ; Use the desired version of GCC
+    (version "14.2.0") ; Use the desired version of GCC
     (source (origin
              (method url-fetch)
              (uri (string-append "http://ftp.gnu.org/gnu/gcc/gcc-" version "/gcc-" version ".tar.gz"))
@@ -36,22 +35,19 @@
               (base32
                "1wd4vl1yd5h9bvnk6ax16ng7j4jmxyfcfzzg5nqwxmcsfjw1dvlr"))))
     (build-system gnu-build-system)
+    (arguments
+      (list 
+	#:out-of-source? #t))
     (inputs (list glibc-2.17))
     (synopsis "The GNU Compiler Collection")
     (description "GCC is the GNU Compiler Collection, which includes front ends for C, C++, and more.")
     (home-page "https://gcc.gnu.org/")
     (license lgpl2.0+)))
 
-;;(define-public gcc-glibc-toolchain
-;;  (package
-;;    (name "gcc-glibc-toolchain")
-;;    (version "1.0")
-;;    (source #f)
-;;    (build-system gnu-build-system)
-;;    (inputs (list gcc-with-glibc))
-;;    (native-inputs (list glibc-2.17))
-;;    (synopsis "Custom GCC toolchain with glibc 2.17")
-;;    (description "A GCC toolchain built with glibc 2.17.")
-;;    (home-page "https://github.com/shen390s")
-;;    (license lgpl2.0+)))
-;;
+(define my-make-gcc-toolchain
+  (module-ref (resolve-module '(gnu packages commencement)) 'make-gcc-toolchain))
+
+(define-public gcc-glibc-toolchain
+  (my-make-gcc-toolchain gcc-14  glibc-2.17)) ;; (interaction-environment)))
+
+gcc-glibc-toolchain
