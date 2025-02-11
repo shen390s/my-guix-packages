9f282fa039e98005ce9bd56cce36a806a0df684c
Rongsong Shen <rongsong.shen@akrostar-tech.com>
2025-02-11 14:09:50 +0800

    update hash code

--- shen390s/packages/xbuild.scm
+++ shen390s/packages/xbuild.scm
@@ -9,18 +9,18 @@
   #:use-module (gnu packages autotools))
 
 (define-public xbuild
-  (let ((commit "2ce5b7705ab12fbc2eacb7672cdb8fe78bbd6504"))
+  (let ((commit "48f0292e89bc54a60781f1f8a8d5f2d1ab22a151"))
     (package
      (name "xbuild")
      (version "0.0.1")
-     (source (origin
-              (method git-fetch)
-	      (uri (git-reference
-                    (url "https://github.com/shen390s/xbuild.git")
-                    (commit commit)))
-              (sha256
-               (base32
-		"0wlip1413caiq5rvbb715l3kmyp4bcjgkknvc1y9anrh7n9q752r"))))
+      (source (origin
+               (method git-fetch)
+      	      (uri (git-reference
+                     (url "https://github.com/shen390s/xbuild.git")
+                     (commit commit)))
+               (sha256
+                (base32
+      		"0w4c39klwrqqbigd3vl2n88hfwlbn0wrgsh2hmlh9dihy0zcprw3"))))
      (build-system gnu-build-system)
      (arguments
       `(#:tests? #f))
