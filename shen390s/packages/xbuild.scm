0aba5614993ed2d2d9bc776625e214ac1ebff621
Rongsong Shen <rongsong.shen@akrostar-tech.com>
2025-02-12 09:25:06 +0800

    update hash code

--- shen390s/packages/xbuild.scm
+++ shen390s/packages/xbuild.scm
@@ -31,7 +31,7 @@
      (inputs (list
 	      autoconf automake libtool))
      (propagated-inputs
-      (list c-capnproto gcc-toolchain-14 gnu-make bash))
+      (list c-capnproto gcc-toolchain-14 gnu-make bash strace))
      (home-page "https://github.com/shen390s/xbuild")
      (synopsis "xbuild")
      (description
