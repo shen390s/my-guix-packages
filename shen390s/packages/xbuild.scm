9fd259126a5b55ab563d264b58522beb8e6e5cc3
Rongsong Shen <rongsong.shen@akrostar-tech.com>
2025-02-21 18:21:28 +0800

    log4c

--- shen390s/packages/xbuild.scm
+++ shen390s/packages/xbuild.scm
@@ -16,7 +16,31 @@
   #:use-module (gnu packages mpi)
   #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages databases)
+  #:use-module (gnu packages gcc)
   #:use-module (gnu packages autotools))
+
+(define-public log4c
+  (let ((commit "7cc4b20dd719530617fea6179872a6c0fe01bf76"))
+    (package
+     (name "log4c")
+     (version "1.2.4")
+     (source (origin
+	      (method git-fetch)
+	      (uri (git-reference
+		    (url "https://github.com/shen390s/log4c.git")
+		    (commit commit)))
+	      (sha256
+	       (base32
+		"1h9jaww7hvq738pf7ckqmj5hqcp3csi4p03r404w3rg86xwbgpa8"))))
+     (build-system gnu-build-system)
+     (arguments
+      `(#:tests? #f))
+     (native-inputs
+      (list autoconf automake libtool curl gcc))
+     (home-page "https://sourceforge.net/projects/log4c/files/log4c/")
+     (synopsis "")
+     (description "log4c is a library for logs for c")
+     (license license:expat))))
 
 (define-public xbuild
   (let ((commit "15a36688831cade542bc61e30d46687943e41aa3"))
@@ -48,3 +72,4 @@
 
 
 ;;xbuild
+;;log4c
