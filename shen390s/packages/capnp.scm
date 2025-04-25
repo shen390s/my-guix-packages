39494c20375c0840000019c47a08f0db41aa3f30
Rongsong Shen <rongsong.shen@akrostar-tech.com>
2025-04-25 10:25:50 +0800

    improve ptr

--- shen390s/packages/capnp.scm
+++ shen390s/packages/capnp.scm
@@ -48,7 +48,7 @@ RPC system.  Think JSON, except binary.  Or think Protocol Buffers, except faste
                     (commit commit)))
               (sha256
                (base32
-		"1c9mmllkng6mjny4ydjhyj72jaz72mxqagy5gv54v27wmrhcr2ki"))))
+		"0fmvq6yk301zqk807s02myhdl5g2p3gyviidqxbyi136dah42y94"))))
      (build-system cmake-build-system)
      (arguments
       `(#:configure-flags (list "--preset=ci-linux_x86_64" "-DBUILD_TESTING=OFF" "-G" "Ninja")
